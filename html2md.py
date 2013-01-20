fc67b56ad4de77c88d5497aac3f2f08cb9987f70
Max Arnold <arnold.maxim@gmail.com>
2013-01-20 16:01:27 +0700

    switch to argparse

--- html2md.py
+++ html2md.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
-import optparse
+import argparse
 
 from html2md import Html2Md, __version__
 
 def main():
-    parser = optparse.OptionParser('%prog (filename|url)', version='%prog ' + __version__)
-    (opts, args) = parser.parse_args()
+    parser = argparse.ArgumentParser(description='HTML to Markdown converter')
+    parser.add_argument('source', help='url or filename')
+    parser.add_argument('-c', '--coding', dest='coding', default='UTF-8',
+                        help='override document encoding')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
+
+    args = parser.parse_args()
 
 if __name__ == "__main__":
     main()
